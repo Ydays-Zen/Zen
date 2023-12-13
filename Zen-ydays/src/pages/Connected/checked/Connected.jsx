18259980287cf5c92efd6c8b4ca16bbd19a33bf3
Keil-ENZO <enzo.keil06@icloud.com>
2023-12-13 10:07:01 +0100

    components

--- Zen-ydays/src/pages/Connected/checked/Connected.jsx
+++ Zen-ydays/src/pages/Connected/checked/Connected.jsx
@@ -3,21 +3,16 @@ import { useContext } from "react";
 import { auth } from "../../../db/firebase-config.jsx";
 import { signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
-import { storage } from "../../../db/firebase-config.jsx";
-import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
-import { v4 } from "uuid";
+
 
 import Comments from "../../../components/Comments.jsx";
 import Header from "../../../layout/Header.jsx";
+import UploadImg from "../../../components/UploadImg.jsx";
 
 const Connected = () => {
   const navigate = useNavigate();
 
   const { currentUser } = useContext(UserContext);
-
-  const [file, setFile] = useState(null);
-  const [imgList, setImgList] = useState([]);
 
   const logOut = async () => {
     try {
@@ -28,36 +23,6 @@ const Connected = () => {
     }
   };
 
-  const uploadImg = () => {
-    if (file == null) return;
-
-    const imgRef = ref(
-      storage,
-      `images/${currentUser.uid}/${file.name + v4()}`
-    );
-
-    uploadBytes(imgRef, file).then((snapshot) => {
-      console.log("Uploaded img");
-      getDownloadURL(snapshot.ref).then((url) => {
-        setImgList((prev) => [...prev, url]);
-      });
-    });
-  };
-
-  useEffect(() => {
-    if (!currentUser) return;
-
-    const userImgListRef = ref(storage, `images/${currentUser.uid}/`);
-
-    listAll(userImgListRef).then((res) => {
-      res.items.forEach((itemRef) => {
-        getDownloadURL(itemRef).then((url) => {
-          setImgList((prev) => [...prev, url]);
-        });
-      });
-    });
-  }, [currentUser]);
-
   return (
     <div>
       <Header />
@@ -65,25 +30,9 @@ const Connected = () => {
       {currentUser && <h2>Welcome {currentUser.email}</h2>}
       {currentUser && <button onClick={logOut}>Log Out</button>}
 
-      <input
-        type="file"
-        name=""
-        id=""
-        onChange={(event) => {
-          setFile(event.target.files[0]);
-        }}
-      />
-      <button onClick={uploadImg}>Upload Img</button>
-
-      {imgList.length > 0 ? (
-        [...new Set(imgList)].map((img, index) => (
-          <img src={img} alt="" key={index} />
-        ))
-      ) : (
-        <h1>No images</h1>
-      )}
-
       <Comments />
+
+      <UploadImg />
     </div>
   );
 };
