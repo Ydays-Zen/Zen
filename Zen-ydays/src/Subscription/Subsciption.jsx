057ed3e5fade46955907244d6b44935e25bf10bb
AxelDC1601 <axel.didiercouzard@ynov.com>
2023-12-13 14:31:21 +0100

    test

--- Zen-ydays/src/Subscription/Subsciption.jsx
+++ Zen-ydays/src/Subscription/Subsciption.jsx
@@ -1,26 +1,23 @@
+// Subsciption.jsx
 import { auth, firestore } from "../db/firebase-config";
-
-firebase.initializeApp(firebaseConfig);
-
-export const auth = firebase.auth();
-export const firestore = firebase.firestore();
+import firebase from 'firebase/compat/app';  // Importez 'compat/app' pour la compatibilité avec Firebase v9
 
 export const followUser = async (currentUserId, targetUserId) => {
   try {
-    // met à jour les abonnements de l'utilisateur actuel
+    // Mettez à jour les abonnements de l'utilisateur actuel
     await firestore.collection('utilisateurs').doc(currentUserId).update({
       abonnements: firebase.firestore.FieldValue.arrayUnion(targetUserId),
     });
 
-    // met à jour les abonnés de l'utilisateur suivi
+    // Mettez à jour les abonnés de l'utilisateur suivi
     await firestore.collection('utilisateurs').doc(targetUserId).update({
       abonnes: firebase.firestore.FieldValue.arrayUnion(currentUserId),
     });
 
     console.log('Abonnement réussi');
   } catch (error) {
-    console.error('Erreur lors de l'abonnement:', error);
+    console.error('Erreur lors de l\'abonnement :', error);
   }
 };
 
-export default Subscription;
+export default followUser;
