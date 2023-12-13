3e021217ed073c8f4f44555554c5c799711a4c5a
AxelDC1601 <axel.didiercouzard@ynov.com>
2023-12-13 14:00:13 +0100

    hugo le kassos

--- Zen-ydays/src/Subscription/Subsciption.jsx
+++ Zen-ydays/src/Subscription/Subsciption.jsx
@@ -1,10 +1,4 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-
-
-const firebaseConfig = {
-  
-};
+import { auth, firestore } from "../db/firebase-config";
 
 firebase.initializeApp(firebaseConfig);
 
@@ -25,8 +19,8 @@ export const followUser = async (currentUserId, targetUserId) => {
 
     console.log('Abonnement réussi');
   } catch (error) {
-    console.error('Erreur lors de l\'abonnement:', error);
+    console.error('Erreur lors de l'abonnement:', error);
   }
 };
 
-export default followUser;
+export default Subscription;
