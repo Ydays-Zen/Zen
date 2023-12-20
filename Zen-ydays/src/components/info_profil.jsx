a859f6926bba8cfb2b099cd0d82dab6ddd9c5904
hugoodn <hugo.donne@ynov.com>
2023-12-20 12:56:49 +0100

    test

--- Zen-ydays/src/components/info_profil.jsx
+++ Zen-ydays/src/components/info_profil.jsx
@@ -1,35 +1,36 @@
+import { useContext } from 'react';
+import { UserContext } from '../context/userContext';
+
 import '../pages/Profil/profil.css';
 import profil_image from '../assets/exemple_pp.jpeg';
 
+export function Info_profil() {
+  // Utilisez le contexte utilisateur
+  const { followerCount, followingCount } = useContext(UserContext);
 
-
-export function Info_profil(){
-    return(
-        <div>
-            <div className="container_info">
-                <div className="image_profil">
-                    <img src={profil_image} alt=""/>
-                </div>
-                <div className="info_profil">
-                    <div className="name">
-                        <h4>Pseudo de du pelo</h4>
-                    </div>
-                    <div className="info_follow">
-                        <div className="followers">
-                            <p>Abonnés</p>
-                            <p>1</p>
-                        </div>
-                        <div className="following">
-                            <p>abonnement</p>
-                            <p>69</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="edit_profil"></div>
-            </div>
+  return (
+    <div>
+      <div className="container_info">
+        <div className="image_profil">
+          <img src={profil_image} alt="" />
         </div>
-    )
+        <div className="info_profil">
+          <div className="name">
+            <h4>Pseudo de du pelo</h4>
+          </div>
+          <div className="info_follow">
+            <div className="followers">
+              Followers: {followerCount}
+            </div>
+            <div className="following">
+              Following: {followingCount}
+            </div>
+          </div>
+        </div>
+        <div className="edit_profil"></div>
+      </div>
+    </div>
+  );
 }
-
 
 export default Info_profil;
