2c8142e8afde294e6a341e849009b3ad1051dda1
Theo-06130 <theo.cerkownik@ynov.com>
2023-12-20 12:27:04 +0100

    début partie abonné

--- Zen-ydays/src/components/info_profil.jsx
+++ Zen-ydays/src/components/info_profil.jsx
@@ -11,10 +11,18 @@ export function Info_profil(){
                     <img src={profil_image} alt=""/>
                 </div>
                 <div className="info_profil">
-                    <div className="name"></div>
+                    <div className="name">
+                        <h4>Pseudo de du pelo</h4>
+                    </div>
                     <div className="info_follow">
-                        <div className="followers"></div>
-                        <div className="following"></div>
+                        <div className="followers">
+                            <p>Abonnés</p>
+                            <p>1</p>
+                        </div>
+                        <div className="following">
+                            <p>abonnement</p>
+                            <p>69</p>
+                        </div>
                     </div>
                 </div>
                 <div className="edit_profil"></div>
