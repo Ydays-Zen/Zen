ae66f769d5d99bfcc7b3dcbcb86e5a2c94b24ec2
Theo-06130 <theo.cerkownik@ynov.com>
2023-12-20 11:40:32 +0100

    ajout des pages pour le profil

--- Zen-ydays/src/pages/Profil/Profil.jsx
+++ Zen-ydays/src/pages/Profil/Profil.jsx
@@ -4,6 +4,8 @@ import "./Profil.css";
 import SubManager from '../../SubManager/SubManager.jsx';
 
 import Info_profil from "../../components/info_profil.jsx";
+import Oeuvres_profil from "../../components/oeuvres_profil.jsx";
+import Last_lecture_profil from "../../components/last_lecture_profil.jsx";
 import Header from "../../layout/Header.jsx";
 import Nav from "../../layout/Nav.jsx";
 function Profil() {
@@ -18,41 +20,9 @@ function Profil() {
     <>
       <Header/>
       <Info_profil/>
+      <Oeuvres_profil/>
+      <Last_lecture_profil/>
       <Nav/>
-      {/*<div className="Profil">
-        <h1>Profil</h1>
-        <div className="Profil-Container">
-          <div className="Profil-Container-Info">
-            <div>
-              <h2>Profil de l&apos;utilisateur</h2>
-              {/* Ajoutez les parenthèses pour appeler correctement handleFollow */}
-              <button onClick={() => handleFollow()}>Follow</button>
-            </div>
-            <div className="Profil-Container-Info-Name">
-              <h2>Nom</h2>
-              <p>Prénom</p>
-            </div>
-            <div className="Profil-Container-Info-Email">
-              <h2>Email</h2>
-              <p>Adresse email</p>
-            </div>
-            <div className="Profil-Container-Info-Password">
-              <h2>Mot de passe</h2>
-              <p>**********</p>
-            </div>
-            <div className="Profil-Container-Info-Edit">
-              <button>Modifier</button>
-            </div>
-          </div>
-          <div className="Profil-Container-Posts">
-            <h2>Posts</h2>
-            <div className="Profil-Container-Posts-Post">
-              <h3>Titre</h3>
-              <p>Contenu</p>
-            </div>
-          </div>
-        </div>
-      </div>*/}
     </>
   );
 }
