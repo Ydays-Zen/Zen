43be5f8dadf87467a969a1870eae6c9d4a2fdd64
Theo-06130 <theo.cerkownik@ynov.com>
2023-12-20 15:16:26 +0100

    fin page profil

--- Zen-ydays/src/components/last_lecture_profil.jsx
+++ Zen-ydays/src/components/last_lecture_profil.jsx
@@ -1,4 +1,5 @@
 import '../pages/Profil/profil.css'
+import exemple_minia from "../assets/exemple_pp.jpeg";
 
 
 export function Last_lecture_profil(){
@@ -7,7 +8,17 @@ export function Last_lecture_profil(){
             <div className="container_lecture">
                 <h3>LECTURE EN COURS</h3>
                 <div className="all_lecture">
-                    <div className="exemple_lecture"></div>
+                    <div className="exemple_lecture">
+                        <img src={exemple_minia} alt=""/>
+                        <img src={exemple_minia} alt=""/>
+                        <img src={exemple_minia} alt=""/>
+                        <img src={exemple_minia} alt=""/>
+                        <img src={exemple_minia} alt=""/>
+                        <img src={exemple_minia} alt=""/>
+                        <img src={exemple_minia} alt=""/>
+                        <img src={exemple_minia} alt=""/>
+                        <img src={exemple_minia} alt=""/>
+                    </div>
                 </div>
             </div>
         </div>
