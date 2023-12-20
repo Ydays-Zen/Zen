65ce153b6e0a3a24c4a5f72888d5864cb29afd96
ibra-sko <ibrahim.sakotraore@gmail.com>
2023-12-20 09:24:07 +0100

    rien

--- Zen-ydays/src/pages/Connected/Check.jsx
+++ Zen-ydays/src/pages/Connected/Check.jsx
@@ -1,12 +1,7 @@
 import { useContext } from 'react';
 import { UserContext } from "../../context/userContext.jsx";
 import { Outlet, useLocation, Navigate } from "react-router-dom";
-<<<<<<< HEAD
-=======
-import Header from "../../layout/Header.jsx";
-import Nav from "../../layout/Nav.jsx";
-import '../../pages/Connected/checked/Check.css';
->>>>>>> 4820c9669889840e48fdc72df872882c039a5bf4
+
 
 const Private = () => {
   const { currentUser } = useContext(UserContext);
