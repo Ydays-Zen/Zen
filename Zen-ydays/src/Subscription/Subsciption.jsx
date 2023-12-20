60d7184f2412755167252408f30a0b142bd9068c
AxelDC1601 <axeldc1601@gmail.com>
2023-12-20 11:37:05 +0100

    test

--- Zen-ydays/src/Subscription/Subsciption.jsx
+++ Zen-ydays/src/Subscription/Subsciption.jsx
@@ -1,17 +1,9 @@
 // Subsciption.jsx
-import { auth, firestore } from "../db/firebase-config";
+import { firestore } from "../db/firebase-config";
 import firebase from 'firebase/compat/app';  // Importez 'compat/app' pour la compatibilité avec Firebase v9
 
 export const followUser = async (currentUserId, targetUserId) => {
   try {
-    // Assurez-vous que l'utilisateur est authentifié
-    if (!auth.currentUser) {
-      // Si l'utilisateur n'est pas authentifié, vous pouvez soit le rediriger vers la page de connexion,
-      // soit afficher un message d'erreur.
-      console.error("Utilisateur non authentifié.");
-      return;
-    }
-
     // Mettez à jour les abonnements de l'utilisateur actuel
     await firestore.collection('utilisateurs').doc(currentUserId).update({
       abonnements: firebase.firestore.FieldValue.arrayUnion(targetUserId),
