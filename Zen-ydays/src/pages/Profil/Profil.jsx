fccca561cf395e727321a9f1a0440b3a9a352968
AxelDC1601 <axel.didiercouzard@ynov.com>
2024-01-03 09:12:41 +0100

    test

--- Zen-ydays/src/pages/Profil/Profil.jsx
+++ Zen-ydays/src/pages/Profil/Profil.jsx
@@ -1,17 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../context/userContext";
-<<<<<<< HEAD
 import { doc, getDoc } from 'firebase/firestore';
 import { firestore } from '../../db/firebase-config.jsx'; // Assurez-vous que le chemin est correct
-=======
-import "./Profil.css";
-import SubManager from "../../SubManager/SubManager.jsx";
-import Info_profil from "../../components/info_profil.jsx";
-import Oeuvres_profil from "../../components/oeuvres_profil.jsx";
-import Last_lecture_profil from "../../components/last_lecture_profil.jsx";
-import Header from "../../layout/home/Header.jsx";
-import Nav from "../../layout/Nav.jsx";
->>>>>>> 8f602c239bc612ce025b82cc82d0beaee84d9dda
 
 function Profil() {
   const currentUserId = useContext(UserContext);
