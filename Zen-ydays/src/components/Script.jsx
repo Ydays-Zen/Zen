dc711a0d6f287e702287b47964aa48e42a2ba258
Keil-ENZO <enzo.keil06@icloud.com>
2024-03-20 16:38:49 +0100

    script

---
+++ Zen-ydays/src/components/Script.jsx
@@ -0,0 +1,85 @@
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../context/userContext";
+import { firestore } from "../db/firebase-config";
+
+const Script = () => {
+  const { currentUser } = useContext(UserContext);
+  const [booksList, setBooksList] = useState([]);
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!currentUser) return;
+
+      const likedBooksRef = collection(firestore, "Books");
+      const q = query(
+        likedBooksRef,
+        where("likedBy", "array-contains", currentUser.uid)
+      );
+      const querySnapshot = await getDocs(q);
+
+      const booksData = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      setBooksList(booksData);
+    };
+
+    fetchData();
+  }, [currentUser]); // Assurez-vous de rappeler fetchData lorsque currentUser change
+
+  useEffect(() => {
+    const fetchSuggestions = async () => {
+      // Extraire les IDs des livres aimés par l'utilisateur
+      const likedBookIds = booksList.map((book) => book.id);
+
+      // Rechercher d'autres livres avec les mêmes tags, mais qui ne sont pas déjà aimés par l'utilisateur
+      const booksRef = collection(firestore, "Books");
+      const q = query(booksRef);
+      const querySnapshot = await getDocs(q);
+
+      const suggestionsData = querySnapshot.docs
+        .map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }))
+        .filter((book) => !likedBookIds.includes(book.id)); // Filtrer les livres déjà aimés
+
+      setSuggestions(suggestionsData);
+    };
+
+    if (booksList.length > 0) {
+      fetchSuggestions();
+    }
+  }, [booksList, currentUser]);
+
+  return (
+    <>
+      <h1>Livres aimés par l'utilisateur</h1>
+
+      {booksList.map((book) => (
+        <div key={book.id} className="book">
+          <h3>{book.title}</h3>
+          <p>{book.author}</p>
+          <p>{book.tags}</p>
+        </div>
+      ))}
+
+      <hr />
+
+      <h2>Suggestions de livres basées sur les tags</h2>
+
+      {suggestions.map((book) => (
+        <div key={book.id} className="book">
+          <h3>{book.title}</h3>
+          <p>{book.author}</p>
+          <p>{book.tags}</p>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Script;
