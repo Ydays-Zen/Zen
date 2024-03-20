fbbd75e0b51b997452314522291ceb1465c54d8b
Enzo Keil <enzokeil@MacBook-Pro-de-Enzo.local>
2024-03-20 10:24:09 +0100

    inputs validation

--- Zen-ydays/src/pages/SignIn/SignIn.jsx
+++ Zen-ydays/src/pages/SignIn/SignIn.jsx
@@ -1,31 +1,69 @@
-import PropTypes from "prop-types";
+import { useContext, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../../context/userContext.jsx";
 
-const UserResult = ({ user }) => {
-  const { displayName, ID } = user; // Renommez ID en ID pour correspondre à la casse
+const SignIn = () => {
+  const inputs = useRef([]);
+  const [validation, setValidation] = useState("");
+  const { signIn } = useContext(UserContext);
+  const formRef = useRef();
+  const navigate = useNavigate();
 
-  // Vérifier si la propriété displayName est définie
-  if (!displayName || !ID) {
-    // Afficher un message ou un rendu alternatif si displayName est manquant
-    return <p>Nom d utilisateur ou IDentifiant manquant.</p>;
-  }
+  const addInputs = (el) => {
+    if (el && !inputs.current.includes(el)) {
+      inputs.current.push(el);
+    }
+  };
 
-  // Rendu normal si displayName est disponible
+  const handleForm = async (e) => {
+    e.preventDefault();
+
+    if (
+      inputs.current[0].value.length == "" ||
+      inputs.current[1].value.length == ""
+    ) {
+      setValidation("Veuillez remplir tous les champs");
+      return;
+    }
+
+    try {
+      const cred = await signIn(
+        inputs.current[0].value,
+        inputs.current[1].value
+      );
+
+      formRef.current.reset();
+      setValidation("");
+      navigate("/check/connected");
+    } catch (error) {
+      setValidation("Email ou mot de passe incorrect");
+    }
+  };
+
   return (
-    <div className="user-result">
-      <h4>{displayName}</h4>
-      <h4>{ID}</h4> {/* Utilisez ID au lieu de ID */}
-      {/* Ajoutez d'autres informations sur l'utilisateur si nécessaire */}
+    <div>
+      <h2>Sign in</h2>
+
+      <form onSubmit={handleForm} ref={formRef}>
+        <input
+          ref={addInputs}
+          type="email"
+          name="email"
+          placeholder="Email"
+          id="signup"
+        />
+        <input
+          ref={addInputs}
+          type="password"
+          name="password"
+          placeholder="Password"
+          id="password"
+        />
+        <p>{validation}</p>
+        <button>Connect</button>
+      </form>
     </div>
   );
 };
 
-// Définition des propTypes pour vérifier les données
-UserResult.propTypes = {
-  user: PropTypes.shape({
-    displayName: PropTypes.string.isRequired,
-    ID: PropTypes.string.isRequired, // Utilisez ID au lieu de ID
-    // Ajoutez d'autres propTypes pour les autres propriétés de l'utilisateur si nécessaire
-  }).isRequired,
-};
-
-export default UserResult;
+export default SignIn;
--- Zen-ydays/src/pages/SignUp/SignUp.jsx
+++ Zen-ydays/src/pages/SignUp/SignUp.jsx
@@ -1,8 +1,8 @@
-import { useState, useRef, useContext, useEffect } from "react";
-import { UserContext } from "../../context/userContext.jsx";
+import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
+import { useContext, useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { auth, firestore } from "../../db/firebase-config.jsx"; // Import your Firebase config
-import { collection, addDoc } from "firebase/firestore";
+import { UserContext } from "../../context/userContext.jsx";
+import { auth, firestore } from "../../db/firebase-config.jsx";
 
 const SignUp = () => {
   const inputs = useRef([]);
@@ -47,20 +47,41 @@ const SignUp = () => {
 
   const handleForm = async (e) => {
     e.preventDefault();
-  
+
     if (inputs.current[2].value !== inputs.current[3].value) {
       setValidation("Les mots de passe ne correspondent pas");
-    } else if (inputs.current[2].value.length < 6 || inputs.current[3].value.length < 6) {
-      setValidation("6 caractères minimum");
+    } else if (
+      inputs.current[2].value.length < 6 ||
+      inputs.current[3].value.length < 6
+    ) {
+      setValidation("Le mot de passe doit avoir 6 caractères minimum");
+    } else if (inputs.current[0].value.length < 3) {
+      setValidation("Le pseudo doit avoir 3 caractères minimum");
+    } else if (
+      !/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(
+        inputs.current[1].value
+      )
+    ) {
+      setValidation("Email invalide");
+    } else if (
+      inputs.current[0].value == "" ||
+      inputs.current[1].value == "" ||
+      inputs.current[2].value == "" ||
+      inputs.current[3].value == ""
+    ) {
+      setValidation("Veuillez remplir tous les champs");
     } else {
       try {
-        const cred = await signUp(inputs.current[1].value, inputs.current[2].value);
-  
+        const cred = await signUp(
+          inputs.current[1].value,
+          inputs.current[2].value
+        );
+
         // Ajoutez la vérification ici pour voir si l'utilisateur existe déjà
         const userRef = collection(firestore, "users");
         const userQuery = query(userRef, where("ID", "==", cred.user.uid));
         const userSnapshot = await getDocs(userQuery);
-  
+
         if (userSnapshot.empty) {
           // L'utilisateur n'existe pas, alors ajoutez-le
           await addDoc(userRef, {
@@ -68,12 +89,12 @@ const SignUp = () => {
             displayName: cred.user.displayName || inputs.current[0].value,
           });
         }
-  
+
         formRef.current.reset();
         setValidation("");
         navigate("/check/connected");
       } catch (error) {
-        // ... (votre code de gestion d'erreur existant)
+        setValidation("Erreur lors de l'inscription");
       }
     }
   };
@@ -83,39 +104,47 @@ const SignUp = () => {
       <h2>Sign up</h2>
 
       <form onSubmit={handleForm} ref={formRef}>
-        <input
-          ref={addInputs}
-          type="text"
-          name="displayName"
-          placeholder="Pseudo"
-          id="displayName"
-          required
-        />
-        <input
-          ref={addInputs}
-          type="email"
-          name="email"
-          placeholder="Email"
-          id="signup"
-          required
-        />
-        <input
-          ref={addInputs}
-          type="password"
-          name="password"
-          placeholder="Password"
-          id="password"
-          required
-        />
-        <input
-          ref={addInputs}
-          type="password"
-          name="confirmPassword"
-          placeholder="Confirm password"
-          id="confirmPassword"
-          required
-        />
+        <div>
+          <input
+            ref={addInputs}
+            type="text"
+            name="displayName"
+            placeholder="Pseudo"
+            id="displayName"
+          />
+        </div>
+
+        <div>
+          <input
+            ref={addInputs}
+            type="email"
+            name="email"
+            placeholder="Email"
+            id="signup"
+          />
+        </div>
+
+        <div>
+          <input
+            ref={addInputs}
+            type="password"
+            name="password"
+            placeholder="Password"
+            id="password"
+          />
+        </div>
+
+        <div>
+          <input
+            ref={addInputs}
+            type="password"
+            name="confirmPassword"
+            placeholder="Confirm password"
+            id="confirmPassword"
+          />
+        </div>
         <p>{validation}</p>
+
         <button type="submit">Submit</button>
       </form>
     </div>
